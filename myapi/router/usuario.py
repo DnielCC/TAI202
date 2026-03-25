e7bbb3d3581e95addd404ac194267618bb0fadd4
DnielCC <123047063@upq.edu.mx>
2026-03-25 10:11:25 -0600

    Endpoints_faltantes

--- myapi/router/usuario.py
+++ myapi/router/usuario.py
@@ -36,46 +36,62 @@ async def agregar_usuarios(usuarioP:crear_usuario,db:Session= Depends(get_db)):
     }
 
 
-@router.put("/",tags=['HTTP CRUD'])
-async def actualizar_usuario(usuario_id: int, usuario: dict):
-    for usr in usuarios:
-        if usr["id"] == usuario_id:
-            usr.update(usuario)
-            return{
-                "mensaje": "Usuario Actualizado",
-                "Datos actualizados": usr,
-            }
-    raise HTTPException(
-        status_code=404,
-        detail="Usuario no encontrado"
-    )
+@router.put("/{usuario_id}",tags=['HTTP CRUD'])
+async def actualizar_usuario(usuario_id: int, usuario: crear_usuario, db:Session= Depends(get_db)):
+    usuario_db = db.query(dbUsuario).filter(dbUsuario.id == usuario_id).first()
+    if not usuario_db:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuario no encontrado"
+        )
+    
+    usuario_db.nombre = usuario.nombre
+    usuario_db.edad = usuario.edad
+    db.commit()
+    db.refresh(usuario_db)
+    
+    return{
+        "mensaje": "Usuario Actualizado",
+        "Datos actualizados": usuario_db,
+    }
 
-@router.patch("/",tags=['HTTP CRUD'])
-async def modificar_usuario(usuario_id: int, usuario: dict):
-    for usr in usuarios:
-        if usr["id"] == usuario_id:
-            usr.update(usuario)
-            return{
-                "mensaje": "Usuario Modificado",
-                "Datos modificados": usr,
-            }
-    raise HTTPException(
-        status_code=404,
-        detail="Usuario no encontrado"
-    )
+@router.patch("/{usuario_id}",tags=['HTTP CRUD'])
+async def modificar_usuario(usuario_id: int, usuario: dict, db:Session= Depends(get_db)):
+    usuario_db = db.query(dbUsuario).filter(dbUsuario.id == usuario_id).first()
+    if not usuario_db:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuario no encontrado"
+        )
+    
+    if "nombre" in usuario:
+        usuario_db.nombre = usuario["nombre"]
+    if "edad" in usuario:
+        usuario_db.edad = usuario["edad"]
+    
+    db.commit()
+    db.refresh(usuario_db)
+    
+    return{
+        "mensaje": "Usuario Modificado",
+        "Datos modificados": usuario_db,
+    }
 
-@router.delete("/",tags=['HTTP CRUD'])
-async def eliminar_usuario(usuario_id: int,usuarioAuth:str=Depends(verificar_peticion)):
-    for i, usr in enumerate(usuarios):
-        if usr["id"] == usuario_id:
-            usuarios.pop(i)
-            return{
-                "mensaje": f"Usuario Eliminado por {usuarioAuth}",
-                "id eliminado": usuario_id,
-            }
-    raise HTTPException(
-        status_code=404,
-        detail="Usuario no encontrado"
-    )
+@router.delete("/{usuario_id}",tags=['HTTP CRUD'])
+async def eliminar_usuario(usuario_id: int, db:Session= Depends(get_db), usuarioAuth:str=Depends(verificar_peticion)):
+    usuario_db = db.query(dbUsuario).filter(dbUsuario.id == usuario_id).first()
+    if not usuario_db:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuario no encontrado"
+        )
+    
+    db.delete(usuario_db)
+    db.commit()
+    
+    return{
+        "mensaje": f"Usuario Eliminado por {usuarioAuth}",
+        "id eliminado": usuario_id,
+    }
 
 
